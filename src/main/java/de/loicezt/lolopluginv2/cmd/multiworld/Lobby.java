ec3c7b968a77e4885a9af8e173cb083337b79cbd
lolocomotive <lolocomotive181@gmail.com>
2020-05-18 14:15:22 +0200

    Added Lobby 1

--- src/main/java/de/loicezt/lolopluginv2/cmd/multiworld/Lobby.java
+++ src/main/java/de/loicezt/lolopluginv2/cmd/multiworld/Lobby.java
@@ -13,11 +13,28 @@ public class Lobby implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player) {
+            try {
+                if (Integer.parseInt(args[0]) == 1) {
+                    Location loc = new Location(Bukkit.getWorld("world"), 155.5, 14, -193.5);
+                    Player p = (Player) sender;
+                    p.teleport(loc);
+                    p.setGameMode(GameMode.ADVENTURE);
+                    Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "lp user " + p.getName() + " parent set lobby");
+                    return true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            } catch (Exception e) {
+                sender.sendMessage("Ooops, looks like something went wrong here, sorry ;(");
+                e.printStackTrace();
+                return true;
+            }
             //return player to the lobby
             Player p = (Player) sender;
-            Location loc = new Location(Bukkit.getWorld("World"), 133.5, 6, -170.5);
+            Location loc = new Location(Bukkit.getWorld("world"), 133.5, 6, -170.5);
             p.teleport(loc);
             p.setGameMode(GameMode.ADVENTURE);
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "lp user " + p.getName() + " parent set lobby");
         } else {
             sender.sendMessage("You must be a player to execute this command");
         }
